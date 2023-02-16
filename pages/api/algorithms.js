f3f60fe6bd84af03b2c90eab042524f6f827ca10
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-02-16 16:45:47 +0800

    algorithms endpoint

---
+++ pages/api/algorithms.js
@@ -0,0 +1,17 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("lalgo");
+ 
+        const algorithms = await db
+            .collection("algorithms")
+            .find({})
+            .toArray();
+ 
+        res.json(algorithms);
+    } catch (e) {
+        console.error(e);
+    }
+ };
